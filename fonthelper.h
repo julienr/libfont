1d4cbf2bd14ff4325d8a8e8262ec96d5ec3813b1
Julien Rebetez <julien@fhtagn.net>
2011-02-14 12:40:02 +0100

    Adds a helper class to draw font

---
+++ fonthelper.h
@@ -0,0 +1,24 @@
+#ifndef _FONT_HELPER_H
+#define _FONT_HELPER_H
+
+#include "font.h"
+
+//A helper class to draw fonts
+class FontHelper {
+  public:
+
+    //Draw a text string of given size centered on coordinates (x,y)
+    static void drawCenteredAt (Font* font, const char* str, float size, float x, float y) {
+      glPushMatrix();
+      float fw, fh;
+      font->getExtent(str, size, &fw, &fh);
+      glTranslatef(x-fw/2.0f, y-fh/2.0f, 0);
+      font->draw(str,size);
+      glPopMatrix();
+    }
+
+  private:
+    FontHelper() {}
+};
+
+#endif
